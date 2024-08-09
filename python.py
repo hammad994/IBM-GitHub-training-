c74cdaded322324a9cdb632e24c8f2b7c52c402b
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2024-08-09 17:59:43 +0000

    reformating the code form the python file

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-# hello form home 
-print("Hello Mr Hammad to our repo here ")
-input("Please , State your busines here .")
-# Thank you 
+# This is the New code Edit 
+X = input("Hello Dear, to the X program. \n What is your name please? ")
+y = input(" What is your department ?")
+print(f"Good to Have you with Us MR {X}, \n Please wait your trun as a member of the {y}")
